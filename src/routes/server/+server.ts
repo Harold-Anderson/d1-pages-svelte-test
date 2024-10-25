a7c39bf9f1017584241ebcc622d557c29af1850e
Harold Anderson <5002519+Harold-Anderson@users.noreply.github.com>
2024-10-24 18:48:06 -0700

    Refactor server.ts and add documentation links

--- src/routes/server/+server.ts
+++ src/routes/server/+server.ts
@@ -1,5 +1,6 @@
 import type { RequestHandler } from "@sveltejs/kit";
 // https://developers.cloudflare.com/pages/functions/bindings/#d1-databases
+// https://developers.cloudflare.com/d1/examples/d1-and-sveltekit/
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET({ request, platform }) {
